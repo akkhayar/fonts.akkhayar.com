60dc331c46cda231cbc51eb5cb351663cd0be88f
Rickaym <psmtitant@gmail.com>
2023-12-04 00:48:52 +0630

    fix: update host url to prod

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-export const HOST = 'http://localhost:5173';
+export const HOST = 'https://fonts.akkhayar.com';
 
 export function generateImportURL(fonts: { family: string, weights?: number[] }[]) {
   return `${HOST}/css2?fonts=${fonts.map((font) => `${font.family}${font.weights ? `:${font.weights.join(',')}` : ''}`).join(';')}&display=swap`;
