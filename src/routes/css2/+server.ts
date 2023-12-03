986ec162504e27e48e5cbc5b1887a965297732c7
Rickaym <psmtitant@gmail.com>
2023-12-04 00:53:52 +0630

    fix: use host variable instead of literal

--- src/routes/css2/+server.ts
+++ src/routes/css2/+server.ts
@@ -1,5 +1,6 @@
 import type { RequestEvent, RequestHandler } from './$types';
 import registeredFonts from '$lib/fonts';
+import { HOST } from '$lib';
 
 
 function parseFontQueryParams(params: URLSearchParams) {
@@ -57,6 +58,6 @@ function fontFaceCSS(family: string, weight: string | undefined, display: string
   font-weight: ${weight || 'normal'};
   font-style: normal;
   font-display: ${display || 'swap'};
-  src: url('http://localhost:5173/s/${firstName}/${weight ? `${weight}/` : ''}${encodeURI(family)}.ttf') format('truetype');
+  src: url('${HOST}/s/${firstName}/${weight ? `${weight}/` : ''}${encodeURI(family)}.ttf') format('truetype');
   unicode-range: U+1000-109F, U+200C-200D, U+25CC, U+A9E0-A9FE, U+AA60-AA7F, U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;\n}`;
 }
