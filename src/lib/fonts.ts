1a59a5c5962d7b6f911cb42ac81e7492fa7635d8
Rickaym <psmtitant@gmail.com>
2023-12-04 21:36:45 +0630

    feat: dynamically register font files

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,3 +1,2 @@
-export default [
-    'Ayar', 'Padauk'
-];
+const fontFiles: { [key: string]: any} = import.meta.glob("../../static/s/**/*.ttf");
+export default Object.keys(fontFiles).map((key) => key.split('/').pop()!.replace('.ttf', ''));
