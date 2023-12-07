779d7f9d802cc22011575d6a5023f867049ab9f2
Rickaym <psmtitant@gmail.com>
2023-12-07 18:52:30 +0630

    refactor: move fonts from /s to /fonts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,2 +1,2 @@
-const fontFiles: { [key: string]: any} = import.meta.glob("../../static/s/**/*.ttf");
+const fontFiles: { [key: string]: any } = import.meta.glob("../../static/fonts/**/*.ttf");
 export default Object.keys(fontFiles).map((key) => key.split('/').pop()!.replace('.ttf', ''));
--- src/routes/css2/+server.ts
+++ src/routes/css2/+server.ts
@@ -51,13 +51,13 @@ export const GET: RequestHandler = async ({ url }: RequestEvent) => {
 }
 
 function fontFaceCSS(family: string, weight: string | undefined, display: string | null) {
-  const firstName = family.split(' ')[0].toLowerCase();
-
+  const encodedFamily = encodeURI(family);
+  
   return `@font-face {
   font-family: '${family}';
   font-weight: ${weight || 'normal'};
   font-style: normal;
   font-display: ${display || 'swap'};
-  src: url('${HOST}/s/${firstName}/${weight ? `${weight}/` : ''}${encodeURI(family)}.ttf') format('truetype');
+  src: url('${HOST}/fonts/${encodedFamily}/${encodedFamily}${weight ? `-${weight}` : ''}.ttf') format('truetype');
   unicode-range: U+1000-109F, U+200C-200D, U+25CC, U+A9E0-A9FE, U+AA60-AA7F, U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;\n}`;
 }
