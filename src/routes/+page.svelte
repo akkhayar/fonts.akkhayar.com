801dff7cc7b3fdb07d915bcbdbfe509f41736d0f
Rickaym <psmtitant@gmail.com>
2023-12-09 18:25:56 +0630

    feat: style changes for responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -70,7 +70,7 @@
                     id="default-checkbox"
                     type="checkbox"
                     value=""
-                    class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    class="w-3 h-3 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
                 <label
                     for="default-checkbox"
@@ -84,7 +84,7 @@
                     id="checked-checkbox"
                     type="checkbox"
                     value=""
-                    class="w-4 h-4 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    class="w-3 h-3 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                 />
                 <label
                     for="checked-checkbox"
@@ -103,9 +103,9 @@
                     <div
                         class="mt-5 my-lang md:mt-0 flex align-center col-span-2"
                     >
-                        <p style="font-size: 20px;">
+                        <!-- <p style="font-size: 20px;">
                             လူခပ်သိမ်း၏ မျိုးရိုးဂုဏ်သိက္ခာနှင့်တကွ...
-                        </p>
+                        </p> -->
                         <p
                             style="font-family: '{font}', sans-serif; font-size: 20px;"
                         >
