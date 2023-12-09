6a1ea5f256270cacb480d90877818c6108c8aa9d
Rickaym <psmtitant@gmail.com>
2023-12-09 16:35:23 +0630

    fix: weights should  be a list of strings

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 export const HOST = 'https://fonts.akkhayar.com';
 
-export function generateImportURL(fonts: { family: string, weights?: number[] }[]) {
+export function generateImportURL(fonts: { family: string, weights?: string[] }[]) {
   return `${HOST}/css2?fonts=${fonts.map((font) => `${font.family}${font.weights ? `:${font.weights.join(',')}` : ''}`).join(';')}&display=swap`;
 }
