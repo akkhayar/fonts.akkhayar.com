cdb2b0ad59b87710f2632fee46babdd429fc99ef
Rickaym <psmtitant@gmail.com>
2023-12-09 16:42:55 +0630

    fix: move function back

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,13 @@
 export const HOST = 'https://fonts.akkhayar.com';
+
+export function generateImportURL(
+    fonts: { family: string; weights?: string[] }[],
+) {
+    return `${HOST}/css2?fonts=${fonts
+        .map(
+            (font) =>
+                `${font.family}${font.weights ? `:${font.weights.join(",")}` : ""
+                }`,
+        )
+        .join(";")}&display=swap`;
+}
